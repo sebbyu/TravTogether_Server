02daf89849a7638224ccd24b796be9be3ebc64cb
sebbyu <you.sebastian.1.618@gmail.com>
2021-03-17 22:13:35 -0700

    fixing heroku deployment

--- travtogether_server/current_settings.py
+++ travtogether_server/current_settings.py
@@ -1,7 +1,7 @@
 
 def __set__(setting):
   local_settings = 'travtogether_server.settings.local'
-  production_settings = 'backend.settings.production'
+  production_settings = 'travtogether_server.settings.production'
   local_host = "127.0.0.1"
   production_host = "travtogether-server.herokuapp.com"
   if setting == "local":
