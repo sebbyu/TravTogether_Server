77ea1600ac7faf66795b4c9a71e6516892f3e67d
sebbyu <you.sebastian.1.618@gmail.com>
2021-03-17 23:39:51 -0700

    p

--- travtogether_server/settings/local.py
+++ travtogether_server/settings/local.py
@@ -7,14 +7,7 @@ DEBUG = True
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'djongo',
-#         'NAME': 'TravTogether',
+#         'NAME': os.path.join(BASE_DIR / '../db.sqlite3'),
 #     }
 # }
 
